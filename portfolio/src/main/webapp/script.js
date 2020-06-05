0a69568a00c8315a20d0afbeecdb5f8df943b121
Austin Teshuba <austinteshuba19@gmail.com>
2020-06-05 16:49:25 -0400

    Remove Front-end Changes

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -98,7 +98,7 @@ function typewriter(textSelector, words, letterDelayMs,
  * response-container div.
  */
 function getData() {
-  fetch('/data?display=2')
+  fetch('/data')
       .then((response) => response.json())
       .then((comments) => {
         // Create a string to contain all of the comments
@@ -106,15 +106,7 @@ function getData() {
 
         // Build up the string with information from each comment.
         for (const comment of comments) {
-          // Start each comment with "Comment: " header
           commentString += "Comment:\n";
-
-          // Use timestamp to display date
-          // String Format: Day_Of_Week Month DD YYYY
-          const timestamp = new Date(comment["timestamp"]);
-          commentString += "Timestamp: " + timestamp.toDateString() + "\n";
-
-          // Populate string with remaining values from object
           commentString += "First Name: " + comment["firstName"] + "\n";
           commentString += "Last Name: " + comment["lastName"] + "\n";
           commentString += "Email: " + comment["email"] + "\n";
