560112cc0c391cffc59c48c3840a8bbdb12e2bfa
Austin Teshuba <austinteshuba19@gmail.com>
2020-06-08 16:57:09 -0400

    Add Blobstore Get Url servlet

---
+++ portfolio/src/main/java/com/google/sps/servlets/BusinessCardServlet.java
@@ -0,0 +1,13 @@
+package com.google.sps.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * This class will handle the storage/retrieval of information from the
+ * Business Card Drop on the portfolio homepage.
+ * TODO - Create this.
+ */
+@WebServlet("/biz-card")
+public class BusinessCardServlet extends HttpServlet {
+}
