37e2e10f1ad1eb6d0ec9dda2fd5649e7681ad0f7
Austin Teshuba <austinteshuba19@gmail.com>
2020-06-08 18:27:54 -0400

    Add Local instances of Blobstore and Datastore

--- portfolio/src/main/java/com/google/sps/servlets/BusinessCardServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/BusinessCardServlet.java
@@ -1,7 +1,15 @@
 package com.google.sps.servlets;
 
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * This class will handle the storage/retrieval of information from the
@@ -10,4 +18,17 @@ import javax.servlet.http.HttpServlet;
  */
 @WebServlet("/biz-card")
 public class BusinessCardServlet extends HttpServlet {
+
+  // Store the BlobstoreService instance for the application. Same instance as other files.
+  private final BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+
+  // Store the Datastore Service instance. Will hold all BizCard entities.
+  private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    //TODO
+  }
+
+
 }
