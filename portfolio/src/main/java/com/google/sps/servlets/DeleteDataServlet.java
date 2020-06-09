d381e720b6e689378490dce38296b7c0da443d73
Austin Teshuba <austinteshuba19@gmail.com>
2020-06-09 10:17:28 -0400

    Fix Wildcard Imports:

--- portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
@@ -14,8 +14,12 @@
 
 package com.google.sps.servlets;
 
-import com.google.appengine.api.datastore.*;
-
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
